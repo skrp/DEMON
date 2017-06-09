049f858496db167cac9ab4982aa8e739664cb4ea
Prince of Archives <rich.tijerina@gmail.com>
2017-06-09 12:45:19 -0600

    Create SHELL.c

--- SHELL.c
+++ SHELL.c
@@ -12,4 +12,20 @@ void daemonize(const char *code)
   pid_t pid;
   struct rlimit rl;
   struct sigaction sa;
-  
+// 1 - clear file creation mask
+  umask(0);
+// 2 - session leader / lose TTY
+  if ((pid = fork()) < 0)
+    { printf("%s: cant fork\n", code); exit(1); }
+  else if (pid != 0)
+    exit(0); // kill parent
+  setsid();
+  sa.sa_handler = SIG_IGN;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  if (sigaction(SIGHUP, &sa, NULL) < 0)
+    { printf("%s: cant ignore SIGHUP", code); exit(1); }
+  if ((pid = fork()) < 0)
+    { printf("%s: cant fork", code); exit(1); }
+  else if (pid != 0)
+     exit(0); // kill parent
